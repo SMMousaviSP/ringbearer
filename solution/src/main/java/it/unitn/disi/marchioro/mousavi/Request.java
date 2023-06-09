817a57706f66cb339ac95aa1a522f1755fab9651
SMMousaviSP <smmousavisp@gmail.com>
2023-06-09 18:08:13 +0200

    Fix format

--- solution/src/main/java/it/unitn/disi/marchioro/mousavi/Request.java
+++ solution/src/main/java/it/unitn/disi/marchioro/mousavi/Request.java
@@ -2,8 +2,13 @@ package it.unitn.disi.marchioro.mousavi;
 
 import scala.collection.immutable.Stream;
 
-enum State{STARTING,PENDING,COMMITTING,ENDING}
-enum Type{READ,UPDATE}
+enum State {
+    STARTING, PENDING, COMMITTING, ENDING
+}
+
+enum Type {
+    READ, UPDATE
+}
 
 public class Request {
     private int key;
@@ -13,16 +18,17 @@ public class Request {
     private Type type;
     private int response_count;
 
-    public Request(int key,String value, Type type) {
-        this.key=key;
-        this.value=value;
-        this.state=State.STARTING;
-        this.locks=0;
+    public Request(int key, String value, Type type) {
+        this.key = key;
+        this.value = value;
+        this.state = State.STARTING;
+        this.locks = 0;
         this.type = type;
-        this.response_count=0;
+        this.response_count = 0;
     }
+
     public Request(int key, Type type) {
-        this(key,"",type);
+        this(key, "", type);
     }
 
     public int getKey() {
@@ -52,20 +58,24 @@ public class Request {
     public void setValue(String value) {
         this.value = value;
     }
-    public int receivedResponse(){
+
+    public int receivedResponse() {
         response_count++;
         return response_count;
     }
-    public int acquiredLock(){
+
+    public int acquiredLock() {
         locks++;
         return locks;
     }
-    public boolean mayBePerformed(){
-        int minimum=type==Type.READ? Constants.R:Constants.W;
-        return locks+Constants.N-response_count>=minimum;
+
+    public boolean mayBePerformed() {
+        int minimum = type == Type.READ ? Constants.R : Constants.W;
+        return locks + Constants.N - response_count >= minimum;
     }
-    public boolean canCommit(){
-        int minimum=type==Type.READ? Constants.R:Constants.W;
-        return locks>=minimum; //maybe should it should be == to avoid multiple commits of the same dataitem
+
+    public boolean canCommit() {
+        int minimum = type == Type.READ ? Constants.R : Constants.W;
+        return locks >= minimum; // maybe should it should be == to avoid multiple commits of the same dataitem
     }
 }
